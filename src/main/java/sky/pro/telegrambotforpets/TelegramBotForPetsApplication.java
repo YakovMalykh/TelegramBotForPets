70dedb73529d04bae428fbcb9398e0e94f13a14f
Sascvotch <sascvotch@yandex.ru>
2022-08-20 07:37:11 +0300

    Создан пустой спринг-бут проект, инициализирована git-flow (созданы ветки master и develop)

--- src/main/java/sky/pro/telegrambotforpets/TelegramBotForPetsApplication.java
+++ src/main/java/sky/pro/telegrambotforpets/TelegramBotForPetsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TelegramBotForPetsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TelegramBotForPetsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotForPetsApplication.class, args);
+    }
 
 }
