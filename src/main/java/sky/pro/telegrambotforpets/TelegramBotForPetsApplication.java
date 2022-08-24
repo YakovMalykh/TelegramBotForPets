cc656d68a61ca259d98a7845f57bd4bd83791942
yakov <to-yakoff@mail.ru>
2022-08-24 18:54:05 +0300

    получил ключ, прописал базовую функциональность бота, подтянул зависимоть

--- src/main/java/sky/pro/telegrambotforpets/TelegramBotForPetsApplication.java
+++ src/main/java/sky/pro/telegrambotforpets/TelegramBotForPetsApplication.java
@@ -2,8 +2,10 @@ package sky.pro.telegrambotforpets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TelegramBotForPetsApplication {
 
     public static void main(String[] args) {
