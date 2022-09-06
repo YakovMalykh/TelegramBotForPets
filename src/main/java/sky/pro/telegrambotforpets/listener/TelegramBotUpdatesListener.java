31867b784aa1a20f166259d293f9f9c01927b8ee
Sascvotch <sascvotch@yandex.ru>
2022-09-06 10:56:55 +0300

    Доработано меню, реализовано получение контакта с занесением в БД

--- src/main/java/sky/pro/telegrambotforpets/listener/TelegramBotUpdatesListener.java
+++ src/main/java/sky/pro/telegrambotforpets/listener/TelegramBotUpdatesListener.java
@@ -4,14 +4,16 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.Keyboard;
-import com.pengrad.telegrambot.model.request.KeyboardButton;
 import com.pengrad.telegrambot.model.request.ParseMode;
-import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sky.pro.telegrambotforpets.interfaces.GuestService;
+import sky.pro.telegrambotforpets.model.Guest;
+import sky.pro.telegrambotforpets.repositories.GuestRepository;
 import sky.pro.telegrambotforpets.services.GuestServiceImpl;
 
 import javax.annotation.PostConstruct;
@@ -30,9 +32,13 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     private TelegramBot telegramBot;
 
     private final GuestService guestService;
+    private final sky.pro.telegrambotforpets.menu.ReplyKeyboard replyKeyboard;
+    private final GuestRepository guestRepository;
 
-    public TelegramBotUpdatesListener(GuestServiceImpl guestService) {
+    public TelegramBotUpdatesListener(GuestServiceImpl guestService, sky.pro.telegrambotforpets.menu.ReplyKeyboard replyKeyboard,GuestRepository guestRepository) {
         this.guestService = guestService;
+        this.replyKeyboard = replyKeyboard;
+        this.guestRepository=guestRepository;
     }
 
     @PostConstruct
@@ -40,82 +46,80 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         telegramBot.setUpdatesListener(this);
     }
 
+    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
     @Override
     public int process(List<Update> updates) {
         updates.forEach(update -> {
-
-            if (!guestService.doesGuestAlreadyExistsInDB(update)) {
-                guestService.saveGuestToDB(update);
+            if (update.message().chat().id() == null) {
+                try {
+                    throw new NoSuchMethodException();
+                } catch (NoSuchMethodException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                Long chatId = update.message().chat().id();
+                if (!guestService.doesGuestAlreadyExistsInDB(update)) {
+                    guestService.saveGuestToDB(update);
+                }
+                if (!(update.message().text() == null)) {
+                    switch (update.message().text()) {
+                        case MENU_1_BUTTON_1 -> {
+                            sendMsg(chatId, "Информация о приюте");
+                            sendMenu(replyKeyboard.Menu0_1(), chatId);
+                        }
+                        case MENU_1_1_BUTTON_1 -> sendMsg(chatId, "Здесь будем выдавать описание приюта");
+                        case MENU_1_1_BUTTON_2 -> sendMsg(chatId, "Расписание и адрес приюта");
+                        case MENU_1_1_BUTTON_3 -> sendMsg(chatId, "Техника безопасности на территории приюта");
+                        case MENU_1_1_BUTTON_4 -> {
+                            sendMsg(chatId, "Оставить контакт");
+                        }
+                        case MENU_1_BUTTON_2 -> {
+                            //Здесь будет ответ на запрос Как взять питомца
+                            sendMsg(chatId, "Как взять питомца из приюта");
+                            sendMenu(replyKeyboard.Menu0_2(), chatId);
+                        }
+                        case MENU_1_2_BUTTON_1 -> sendMsg(chatId, "Правила знакомства с питомцем");
+                        case MENU_1_2_BUTTON_2 -> sendMsg(chatId, "Документы необходимые, чтобы взять питомца");
+                        case MENU_1_2_BUTTON_3 -> sendMsg(chatId, "Рекомендации по транспортировке питомца");
+                        case MENU_1_2_BUTTON_4 -> sendMsg(chatId, "Рекомендации по обустройству дома для щенка");
+                        case MENU_1_2_BUTTON_5 ->
+                                sendMsg(chatId, "Рекомендации по обустройству дома для взрослой собаки");
+                        case MENU_1_2_BUTTON_6 ->
+                                sendMsg(chatId, "Рекомендации по обустройству дома для собаки с ограниченными возможностями");
+                        case MENU_1_2_BUTTON_7 -> sendMsg(chatId, "Советы кинолога по первичному общению с питомцем");
+                        case MENU_1_2_BUTTON_8 -> sendMsg(chatId, "Список проверенных кинологов");
+                        case MENU_1_2_BUTTON_9 -> sendMsg(chatId, "Причины отказа в заборе питомца");
+                        case MENU_1_BUTTON_3 -> {
+                            //Здесь будет Отправка отчета
+                            sendMsg(chatId, "Отправить отчет о питомце");
+                        }
+                        case MENU_1_BUTTON_4 -> {
+                            //Здесь будем звать волонтера
+                            sendMsg(chatId, "Позвать волонтера");
+                        }
+                        default -> {
+                            //В ответ на неопознанную команду выдает меню
+                            logger.info("Неккоректная команда");
+                            sendMenu(replyKeyboard.Menu0(), chatId);
+                        }
+                    }
+                }
             }
-            switch (update.message().text()) {
-                case MENU_1_BUTTON_1:
-                    sendMsg(update, "Информация о приюте");
-                    sendMenu(Menu0_1(), update.message().chat().id());
-                    break;
-                case MENU_1_1_BUTTON_1:
-                    sendMsg(update, "Здесь будем выдавать описание приюта");
-                    break;
-                case MENU_1_1_BUTTON_2:
-                    sendMsg(update, "Расписание и адрес приюта");
-                    break;
-                case MENU_1_1_BUTTON_3:
-                    sendMsg(update, "Техника безопасности на территории приюта");
-                    break;
-                case MENU_1_1_BUTTON_4:
-                    sendMsg(update, "Оставить контакт");
-                    break;
-                case MENU_1_BUTTON_2:
-                    //Здесь будет ответ на запрос Как взять питомца
-                    sendMsg(update, "Как взять питомца из приюта");
-                    sendMenu(Menu0_2(), update.message().chat().id());
-                    break;
-                case MENU_1_2_BUTTON_1:
-                    sendMsg(update, "Правила знакомства с питомцем");
-                    break;
-                case MENU_1_2_BUTTON_2:
-                    sendMsg(update, "Документы необходимые, чтобы взять питомца");
-                    break;
-                case MENU_1_2_BUTTON_3:
-                    sendMsg(update, "Рекомендации по транспортировке питомца");
-                    break;
-                case MENU_1_2_BUTTON_4:
-                    sendMsg(update, "Рекомендации по обустройству дома для щенка");
-                    break;
-                case MENU_1_2_BUTTON_5:
-                    sendMsg(update, "Рекомендации по обустройству дома для взрослой собаки");
-                    break;
-                case MENU_1_2_BUTTON_6:
-                    sendMsg(update, "Рекомендации по обустройству дома для собаки с ограниченными возможностями");
-                    break;
-                case MENU_1_2_BUTTON_7:
-                    sendMsg(update, "Советы кинолога по первичному общению с питомцем");
-                    break;
-                case MENU_1_2_BUTTON_8:
-                    sendMsg(update, "Список проверенных кинологов");
-                    break;
-                case MENU_1_2_BUTTON_9:
-                    sendMsg(update, "Причины отказа в заборе питомца");
-                    break;
-                case MENU_1_BUTTON_3:
-                    //Здесь будет Отправка отчета
-                    sendMsg(update, "Отправить отчет о питомце");
-                    break;
-                case MENU_1_BUTTON_4:
-                    //Здесь будем звать волонтера
-                    sendMsg(update, "Позвать волонтера");
-                    break;
-                default:
-                    //В ответ на неопознанную команду выдает меню
-                    sendMenu(Menu0(), update.message().chat().id());
-                    break;
+            if (!(update.message().contact() == null)) {
+                Guest guest=new Guest();
+                guest.setUserName(update.message().contact().firstName());
+                guest.setChatId(update.message().contact().userId());
+                guest.setPhoneNumber(update.message().contact().phoneNumber());
+                guestRepository.save(guest);
+                logger.info("Данные о госте занесены в БД");
+                System.out.println(update.message().contact());
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
-    public void sendMsg(Update update, String text) {
-        Long chatId = update.message().chat().id();
+    public void sendMsg(Long chatId, String text) {
         SendMessage message = new SendMessage(chatId, text);
         SendResponse responce = telegramBot.execute(message);
         System.out.println(responce.isOk());
@@ -124,54 +128,19 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     }
 
     private void sendMenu(Keyboard Menu, long chatId) {
-        Keyboard replyKeyboardMarkup = Menu;
         SendMessage request = new SendMessage(chatId, "Выберите пункт меню")
-                .replyMarkup(replyKeyboardMarkup)
+                .replyMarkup(Menu)
                 .parseMode(ParseMode.HTML)
                 .disableWebPagePreview(true);
         SendResponse sendResponse = telegramBot.execute(request);
+        logger.info("Меню отправлено" + Menu);
         if (!sendResponse.isOk()) {
             int codeError = sendResponse.errorCode();
             String description = sendResponse.description();
+
         }
+
     }
 
-    private Keyboard Menu0() {
-        Keyboard replyKeyboardMarkup = new ReplyKeyboardMarkup(
-                new String[]{MENU_1_BUTTON_1, MENU_1_BUTTON_2},
-                new String[]{MENU_1_BUTTON_3, MENU_1_BUTTON_4})
-                .oneTimeKeyboard(true)
-                .resizeKeyboard(true)
-                .selective(true);
-        return replyKeyboardMarkup;
-    }
 
-    private Keyboard Menu0_1() {
-        Keyboard replyKeyboardMarkup = new ReplyKeyboardMarkup(
-                new String[]{MENU_1_1_BUTTON_1, MENU_1_1_BUTTON_2},
-                new String[]{MENU_1_1_BUTTON_3},
-                new String[]{MENU_1_1_BUTTON_4},
-                new String[]{MENU_1_BUTTON_3, MENU_1_BUTTON_4})
-                .oneTimeKeyboard(true)
-                .resizeKeyboard(true)
-                .selective(true);
-        return replyKeyboardMarkup;
-    }
-
-    private Keyboard Menu0_2() {
-        Keyboard replyKeyboardMarkup = new ReplyKeyboardMarkup(new KeyboardButton(MENU_1_2_BUTTON_1))
-                .addRow(new KeyboardButton(MENU_1_2_BUTTON_2))
-                .addRow(new KeyboardButton(MENU_1_2_BUTTON_3))
-                .addRow(new KeyboardButton(MENU_1_2_BUTTON_4))
-                .addRow(new KeyboardButton(MENU_1_2_BUTTON_5))
-                .addRow(new KeyboardButton(MENU_1_2_BUTTON_6))
-                .addRow(new KeyboardButton(MENU_1_2_BUTTON_7))
-                .addRow(new KeyboardButton(MENU_1_2_BUTTON_8))
-                .addRow(new KeyboardButton(MENU_1_2_BUTTON_9))
-                .addRow(new KeyboardButton(MENU_1_BUTTON_3), new KeyboardButton(MENU_1_BUTTON_4))
-                .oneTimeKeyboard(true)
-                .resizeKeyboard(true)
-                .selective(true);
-        return replyKeyboardMarkup;
-    }
 }
