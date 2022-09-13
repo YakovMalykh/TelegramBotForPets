697d670b578194f856c94f8aadec87fc11af52fd
yakov <to-yakoff@mail.ru>
2022-09-13 13:44:27 +0300

    решил вопрос с миграциями

--- src/main/resources/liquibase/scripts/dog_cat_adopters.sql
+++
@@ -1,7 +0,0 @@
--- liquibase formatted sql
-
--- changeset ymalykh:1
-ALTER TABLE cat ALTER COLUMN birthday TYPE DATE;
-ALTER TABLE dog ALTER COLUMN birthday TYPE DATE;
-ALTER TABLE dog_adopter ALTER COLUMN birthday TYPE DATE;
-ALTER TABLE cat_adopter ALTER COLUMN birthday TYPE DATE;
